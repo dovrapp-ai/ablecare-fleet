741d619ad7d7216b1b708911f79495366e35f3d8
David Rappaport <dablecare@gmail.com>
2026-08-16 17:56:59 -0400

    Send helper rides to driver app separately

--- driver-sw.js
+++ driver-sw.js
@@ -1,4 +1,4 @@
-const CACHE='ablecare-driver-v4';
+const CACHE='ablecare-driver-v5';
 const SHELL=['/driver.html','/driver-manifest.json','/driver-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
