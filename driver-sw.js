78b15457e9eb75b449e653ec2b8e1b5ddaba6bc8
David Rappaport <dablecare@gmail.com>
2026-08-18 07:55:10 -0400

    Add driver hours navigation notes and trip progress

--- driver-sw.js
+++ driver-sw.js
@@ -1,4 +1,4 @@
-const CACHE='ablecare-driver-v8';
+const CACHE='ablecare-driver-v9';
 const SHELL=['/driver.html','/driver-manifest.json','/driver-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
